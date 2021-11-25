cc7cb28504282957bc2889fe001dd050e63a879a
darshini418 <priyadarshinireddy5@gmail.com>
2021-11-25 11:44:18 +0530

    added the file

---
+++ Std.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Std{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+double y=sc.nextDouble();
+sc.nextLine();
+String s=sc.nextLine();
+System.out.println("String:"+s);
+System.out.println("double:"+y);
+System.out.println("Int:"+x);
+}
+}
+
