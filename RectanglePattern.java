78145a0c280b90a7eeefe3865e019d0ecee0d1e9
darshini418 <priyadarshinireddy5@gmail.com>
2021-12-22 18:45:02 +0530

    added the filename

---
+++ RectanglePattern.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class RectanglePattern{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int N = sc.nextInt();
+for(int i=1;i<4;i++){
+for(int j=1;j<6;j++)
+System.out.print("+ ");
+System.out.println(" ");
+}
+}
+}
