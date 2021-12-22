d3907854182bf5ec72bf18878a26e0aa2841bb1b
darshini418 <priyadarshinireddy5@gmail.com>
2021-12-22 19:13:38 +0530

    added the filename

---
+++ Solidhalfdaimond.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+public class Solidhalfdaimond{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int N = sc.nextInt();
+int M = sc.nextInt();
+for(int i=1;i<N;i++){
+for(int j=1;j<=i;j++)
+System.out.print( "*" );
+System.out.println(" ");
+}
+for(int i=1;i<M;i++){
+for(int j=M-1;j>=i;j--)
+System.out.print("*");
+System.out.println("");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
