ee600e5cd608be40f743d5cedb8f78edc36f402e
darshini418 <priyadarshinireddy5@gmail.com>
2021-12-23 10:51:08 +0530

    added the filename

---
+++ Halfpyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Halfpyramid{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int rows = sc.nextInt();
+sc.close();
+printpattern(rows);
+}
+public static void printpattern(int rows){
+for(int i=1;i<=rows;++i){
+    for(int j=1;j<=i;++j){
+System.out.print(j+" ");
+}
+System.out.println();
+}
+}
+}
+
