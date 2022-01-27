e7cb5f052e4977fc4445895203c67f61546603c0
darshini418 <priyadarshinireddy5@gmail.com>
2022-01-27 19:21:09 +0530

    added the filename

---
+++ Day14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+public class Day14{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+String A = sc.nextLine();
+String B = A.replaceAll("\\s+"," ");
+System.out.println(B);
+}
+}
