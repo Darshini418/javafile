2e4d7a1db2e768e9d4232ecd3d481125a1aeb08e
darshini418 <priyadarshinireddy5@gmail.com>
2022-01-27 19:30:19 +0530

    added the filename

---
+++ Day14ex6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+public class Day14ex6{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+String A = sc.nextLine();
+String B = sc.nextLine();
+System.out.println(A+B);
+}
+}
