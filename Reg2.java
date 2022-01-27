c52acb7f30e96942acf9757b148232f6ece09f4b
darshini418 <priyadarshinireddy5@gmail.com>
2022-01-27 18:42:46 +0530

    added the filename

---
+++ Reg2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+public class Reg2{
+public static void main(String args[]){
+final String REGEX_PATTERN = " !{3}$";
+Pattern r = Pattern.compile(REGEX_PATTERN);
+try{
+FileReader fr = new FileReader("C:\\Users\\Indira\\Desktop\\work\\data2.txt ");
+BufferedReader br = new BufferedReader(fr);
+String line;
+while((line = br.readLine()) != null){
+Matcher m = r.matcher(line);
+if(m.find()){
+System.out.println(line);
+}
+}
+br.close();
+}catch(IOException e){
+System.out.println(e);
+}
+}
+}
